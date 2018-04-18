515a31d037ff962adb79f0d5e2ba41bafe957aef
Briseno.Fernanda <Briseno.Fernanda@SEMS-259-07.SEMS259.LAB>
2018-04-18 15:50:33 -0700

    ChangeCalculator.java-src

---
+++ src/RandomNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class RandomNumbers {
+public static void main(String[] args) {
+Random randy= new Random(); 
+int x= randy.nextInt(51);
+System.out.println(x); 
+int x1= randy.nextInt(25); 
+int x11=randy.nextInt(126)+25; 
+int x111=randy.nextInt(326)+137;
+}
+}
+ 
